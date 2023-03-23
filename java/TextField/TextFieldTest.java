a5032a19720752c58036ecd69c31794c72b4ba62
LukasKinloch <111461606+LukasKinloch@users.noreply.github.com>
2023-03-23 16:05:59 -0400

    Add files via upload

---
+++ java/TextField/TextFieldTest.java
@@ -0,0 +1,14 @@
+/*ViMO*/
+
+import javax.swing.JFrame;
+
+public class TextFieldTest {
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setLocationRelativeTo(null);
+        textFieldFrame.setResizable(false);
+        textFieldFrame.setVisible(true);
+    }
+}
